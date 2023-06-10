ebee276e6f357e2c4116f6c937d0c9b067aa53d1
tetsuro takamura <tetsuro.takamura@gmail.com>
2023-06-10 21:51:12 +0900

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-import ReactDOM from "react-dom";
-import React, { useState } from "react";
-
-const App = () => {
-  const [isActive, setIsActive] = useState(true);
-
-  const handleChangeColor = () => {
-    setIsActive(!isActive);
-  };
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
-    >
-      <button
-        style={{
-          color: isActive ? "#fff" : "#000",
-          backgroundColor: isActive ? "#008000" : "#ff0000",
-          width: "150px",
-          borderRadius: "24px",
-          padding: "10px",
-          cursor: "pointer",
-        }}
-        onClick={handleChangeColor}
-      >
-        {isActive ? "ON" : "OFF"}
-      </button>
-    </div>
-  );
-};
-export default App;
+export default function App() {
+  return <div></div>;
+}
