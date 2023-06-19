b40f649c8c6093c5ec581ea8ea376efd544e8784
tetsuro takamura <tetsuro.takamura@gmail.com>
2023-06-19 19:55:13 +0900

    add:start_code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-import React, { useState } from "react";
-
 export default function App() {
-  const [isActive, setIsActive] = useState(false);
-  console.log(isActive);
-  const toggleIsActive = () => {
-    setIsActive(!isActive);
-    console.log(isActive);
-  };
-
-  return (
-    <div>
-      <button
-        onClick={toggleIsActive}
-        style={{
-          color: isActive ? "#fff" : "#000",
-          backgroundColor: isActive ? "#008000" : "#ff0000",
-        }}
-      >
-        {isActive ? "ON" : "OFF"}
-      </button>
-    </div>
-  );
+  return <div></div>;
 }
