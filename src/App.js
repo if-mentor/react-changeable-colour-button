82f41eadc339ebfbbe64139f626a46821a3bb17c
tetsuro takamura <tetsuro.takamura@gmail.com>
2023-06-19 19:51:29 +0900

    add:completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function App() {
   const [isActive, setIsActive] = useState(false);
@@ -9,12 +9,28 @@ export default function App() {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <button
         onClick={toggleIsActive}
         style={{
           color: isActive ? "#fff" : "#000",
           backgroundColor: isActive ? "#008000" : "#ff0000",
+          borderRadius: "10px",
+          padding: "10px 20px",
+          border: "none",
+          fontFamily: "Arial, sans-serif",
+          fontSize: "16px",
+          cursor: "pointer",
+          transition: "all 0.3s ease",
+          width: "100px", // ボタンの幅を固定
+          height: "50px", // ボタンの高さを固定
         }}
       >
         {isActive ? "ON" : "OFF"}
